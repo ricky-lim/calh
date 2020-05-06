718d93fec3035ffc0c5922d72e99ee27b8602457
ricky-lim <rlim.email@gmail.com>
2020-05-06 15:57:10 +0200

    Bump version: 2.0.0 → 2.1.0

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ricky-lim/calh",
-    version="2.0.0",
+    version="2.1.0",
     zip_safe=False,
 )
